b3b0d8b13fa298b2ddd5a8423d4a68c98ca9ce10
Roberto <rlovece@hotmail.co.uk>
2021-08-14 21:54:22 +0100

    Setup webpack and three js

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  mode: 'development',
+
+  entry: {
+    index: './src/index.js',
+    //print: './src/print.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    }),
+  ],
+
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 8080
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+};
